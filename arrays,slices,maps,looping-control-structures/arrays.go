7ea602ebd5ab2b0cd237abb3e7bc8df5c085362c
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-07-14 12:16:48 -0600

    Add files via upload

---
+++ arrays,slices,maps,looping-control-structures/arrays.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println(intArr)
+
+	var intSlice []int32 = []int32{4, 5, 6}
+	fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
+	intSlice = append(intSlice, 7)
+	fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
+
+	var intSlice2 []int32 = []int32{8, 9}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Println(intSlice)
+
+	var intSlice3 []int32 = make([]int32, 3, 8)
+	fmt.Printf("intSlice3: %v, length: %v, capacity: %v\n", intSlice3, len(intSlice3), cap(intSlice3))
+}
