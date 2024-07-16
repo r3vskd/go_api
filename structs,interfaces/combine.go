9e7d563132d55e9ba5ed65e2ea10521dbf6b21df
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-07-15 23:32:54 -0600

    Update and rename a to combine.go

---
+++ structs,interfaces/combine.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Rectangle struct {
+	Width, Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func printShapeInfo(s Shape) {
+	fmt.Printf("Area: %.2f\n", s.Area())
+	fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
+}
+
+func main() {
+	circle := Circle{Radius: 5}
+	rectangle := Rectangle{Width: 4, Height: 7}
+
+	fmt.Println("Circle Info:")
+	printShapeInfo(circle)
+
+	fmt.Println("\nRectangle Info:")
+	printShapeInfo(rectangle)
+}
